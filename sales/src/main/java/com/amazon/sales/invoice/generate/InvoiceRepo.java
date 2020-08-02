ec3c378e60c4a3499161145fec951b5bb5dc59f3
Parul Geete <parul.geete@gmail.com>
2020-08-02 19:32:03 +0530

    test

--- sales/src/main/java/com/amazon/sales/invoice/generate/InvoiceRepo.java
+++
@@ -1,9 +0,0 @@
-package com.amazon.sales.invoice.generate;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceRepo extends JpaRepository<InvoiceEntity, Integer> {
-
-}
